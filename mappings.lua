d36d8445b858388b64c1a84eab79289dab74fd79
husnawan21 <husnawandwi@gmail.com>
2023-05-05 06:21:57 +0700

    updated with nvchad pojok kode keymaps (few vscode keymaps), etc

--- mappings.lua
+++ mappings.lua
@@ -11,7 +11,9 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
@@ -20,9 +22,57 @@ return {
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
+    -- FROM NVCHAD POJOK KODE
+    ["<C-]>"] = { function() require("Comment.api").toggle.linewise.current() end, desc = "Toggle Linewise Current" },
+    ["<c-c>"] = { '"+y', desc = " " },
+    ["<c-v>"] = { '"+p', desc = " " },
+    ["<S-Down>"] = { "<cmd>t.<cr>", desc = " " },
+    ["<S-Up>"] = { "<cmd>t -1<cr>", desc = " " },
+    ["<M-J>"] = { "<cmd>t.<cr>", desc = " " },
+    ["<M-K>"] = { "<cmd>t -1<cr>", desc = " " },
+    ["<M-Down>"] = { "<cmd>m+<cr>", desc = " " },
+    ["<M-Up>"] = { "<cmd>m-2<cr>", desc = " " },
+    ["<M-j>"] = { "<cmd>m+<cr>", desc = " " },
+    ["<M-k>"] = { "<cmd>m-2<cr>", desc = " " },
+    ["<C-f>"] = { "<cmd>lua vim.lsp.buf.format{async=true}<cr>", desc = " " },
+    ["q"] = { "<cmd>q<cr>", desc = " " },
+    ["<leader>bb"] = { "<cmd>tabnew<cr>", desc = "New Tab" },
+    -- ["<leader>bc"] = { "<cmd>BufferLinePickClose<cr>", desc = " " },
+    -- ["<leader>bj"] = { "<cmd>BufferLinePick<cr>", desc = " " },
+    -- ["<leader>bt"] = { "<cmd>BufferLineSortByTabs<cr>", desc = " " },
   },
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
+  },
+
+  -- FROM NVCHAD POJOK KODE
+  i = {
+    ["<c-c>"] = { '"+y', desc = " " },
+    ["<c-v>"] = { "<c-r>+", desc = " " },
+    ["<S-Down>"] = { "<cmd>t.<cr>", desc = " " },
+    ["<M-Down>"] = { "<cmd>m+<cr>", desc = " " },
+    ["<S-Up>"] = { "<cmd>t -1<cr>", desc = " " },
+    ["<M-Up>"] = { "<cmd>m-2<cr>", desc = " " },
+    ["<C-s>"] = { "<cmd>w<cr>", desc = " " },
+    ["<C-l>"] = { "<cmd>LiveServer start<cr><cr>", desc = " " },
+    ["<C-f>"] = { "<cmd>lua vim.lsp.buf.format{async=true}<cr>", desc = " " },
+  },
+  -- FROM NVCHAD POJOK KODE
+  v = {
+	["<C-]>"] = { "<esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<cr>", desc = " " },
+    ["<A-j>"] = { ":m .+1<CR>==", desc = " " },
+    ["<A-k>"] = { ":m .-2<CR>==", desc = " " },
+    ["p"] = { '"_dP', desc = " " },
+    ["<c-c>"] = { '"+y', desc = " " },
+    ["<c-v>"] = { '"+p', desc = " " },
+    ["J"] = { ":move '>+1<CR>gv-gv", desc = " " },
+    ["K"] = { ":move '<-2<CR>gv-gv", desc = " " },
+    -- ["<A-j>"] = { ":move '>+1<CR>gv-gv", desc = " " },
+    ["<A-Down>"] = { ":move '>+1<CR>gv-gv", desc = " " },
+    -- ["<A-k>"] = { ":move '<-2<CR>gv-gv", desc = " " },
+    ["<A-Up>"] = { ":move '<-2<CR>gv-gv", desc = " " },
+    ["<S-Down>"] = { ":'<,'>t'><cr>", desc = " " },
   },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,3 +1,4 @@
+-- TODO: test
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
@@ -9,4 +10,29 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end,
+    -- opts = {},
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+    keys = {
+      { "<leader>T", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
+    },
+  },
+  {
+    "loctvl842/monokai-pro.nvim",
+    config = function()
+      require("monokai-pro").setup {
+        filter = "octagon", -- classic | octagon | pro | machine | ristretto | spectrum
+      }
+    end,
+  },
 }
