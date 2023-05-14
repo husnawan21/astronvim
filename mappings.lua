b1f1be0c79c11e832d2c5d8547e99ee5e3c2ac7a
husnawan21 <husnawandwi@gmail.com>
2023-05-14 07:20:25 +0700

    change mappings

--- mappings.lua
+++ mappings.lua
@@ -23,10 +23,13 @@ return {
     -- quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
 
+    ["<C-u>"] = { "<C-u>zz", desc = "Scroll up center" },
+    ["<C-d>"] = { "<C-d>zz", desc = "Scroll down center" },
+
     -- FROM NVCHAD POJOK KODE
     ["<C-]>"] = { function() require("Comment.api").toggle.linewise.current() end, desc = "Toggle Linewise Current" },
-    ["<c-c>"] = { '"+y', desc = " " },
-    ["<c-v>"] = { '"+p', desc = " " },
+    -- ["<c-c>"] = { '"+y', desc = " " },
+    -- ["<c-v>"] = { '"+p', desc = " " },
     ["<S-Down>"] = { "<cmd>t.<cr>", desc = " " },
     ["<S-Up>"] = { "<cmd>t -1<cr>", desc = " " },
     ["<M-J>"] = { "<cmd>t.<cr>", desc = " " },
@@ -36,16 +39,16 @@ return {
     ["<M-j>"] = { "<cmd>m+<cr>", desc = " " },
     ["<M-k>"] = { "<cmd>m-2<cr>", desc = " " },
     ["<leader>bf"] = { "<cmd>lua vim.lsp.buf.format{async=true}<cr>", desc = "Format buffer" },
-    ["q"] = { "<cmd>q<cr>", desc = " " },
+    -- ["q"] = { "<cmd>q<cr>", desc = " " },
     ["<leader>bb"] = { "<cmd>tabnew<cr>", desc = "New Tab" },
     -- ["<leader>bc"] = { "<cmd>BufferLinePickClose<cr>", desc = " " },
     -- ["<leader>bj"] = { "<cmd>BufferLinePick<cr>", desc = " " },
     -- ["<leader>bt"] = { "<cmd>BufferLineSortByTabs<cr>", desc = " " },
   },
-  t = {
-    -- setting a mapping to false will disable it
-    -- ["<esc>"] = false,
-  },
+  -- t = {
+  -- setting a mapping to false will disable it
+  -- ["<esc>"] = false,
+  -- },
 
   -- FROM NVCHAD POJOK KODE
   i = {
@@ -65,8 +68,8 @@ return {
     ["<A-j>"] = { ":m .+1<CR>==", desc = " " },
     ["<A-k>"] = { ":m .-2<CR>==", desc = " " },
     ["p"] = { '"_dP', desc = " " },
-    ["<c-c>"] = { '"+y', desc = " " },
-    ["<c-v>"] = { '"+p', desc = " " },
+    -- ["<c-c>"] = { '"+y', desc = " " },
+    -- ["<c-v>"] = { '"+p', desc = " " },
     ["J"] = { ":move '>+1<CR>gv-gv", desc = " " },
     ["K"] = { ":move '<-2<CR>gv-gv", desc = " " },
     -- ["<A-j>"] = { ":move '>+r<CR>gv-gv", desc = " " },
